45d7a6df30155ec3342536aee3e86396dd78276e
endtm <kiraira19850@gmail.com>
2025-07-22 18:30:16 +0330

    Update binary files and assembly information for AccountingApp; change AssemblyInformationalVersion and sourcelink reference for consistency in versioning.

--- AccountingApp/obj/Debug/net6.0-windows/AccountingApp.AssemblyInfo.cs
+++ AccountingApp/obj/Debug/net6.0-windows/AccountingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d98580a72402dfeeff2f20aaedab9dae9cf29f91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be0c02d5ff030914c479d03c74aca0db574a538")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
